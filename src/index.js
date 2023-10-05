d131178508a7fb17bd2cc23e2d015d8680337ecf
yugo <yugosakamoto0717@gmail.com>
2023-10-05 17:38:27 -0300

    [setup] Installs projects dependecies and project architecture

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.listen(process.env.PORT, () => {
+  console.log("Server running at port " + process.env.PORT);
+});
